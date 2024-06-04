8580cbd9a87c4ac30bdea3fc5e8458674234cd9f
mingqishan <98464053+mingqishan@users.noreply.github.com>
2024-06-05 01:35:50 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -25,7 +25,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),pdx215))
+ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),pdx214))
     subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
     $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
